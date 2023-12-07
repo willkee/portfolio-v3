417d5b4f55172a6b6bce7f575abad9dd00e14453
Will Kee <william.w.kee@gmail.com>
2023-12-07 11:22:44 -0700

    contact form framework

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,22 @@
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import styles from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+
 const Contact = () => {
-	return null;
+	const formRef = useRef();
+	const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+	const [loading, setLoading] = useState(false);
+
+	const handleChange = (e) => {};
+	const handleSubmit = (e) => {};
+
+	return <div></div>;
 };
 
 export default Contact;
